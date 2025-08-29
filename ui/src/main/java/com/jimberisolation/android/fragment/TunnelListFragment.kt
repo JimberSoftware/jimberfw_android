dd8ed235566121dc93e339388744036212c181e9
Lennert <lennert.defauw@jimber.org>
2025-08-29 12:45:50 +0200

    fix: default no edit, except if you tap 5 times

--- ui/src/main/java/com/jimberisolation/android/fragment/TunnelListFragment.kt
+++ ui/src/main/java/com/jimberisolation/android/fragment/TunnelListFragment.kt
@@ -13,8 +13,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
